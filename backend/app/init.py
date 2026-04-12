68ba6701377e0690273303e2f66e71d299a9b3e5
Emilien Klein <emilien@klein.st>
2026-04-12 15:39:27 +0200

    Index page

--- backend/app/init.py
+++ backend/app/init.py
@@ -1,11 +1,15 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
 
+    if os.environ.get("FLASK_ENV") == "development":
+        app.config["TEMPLATES_AUTO_RELOAD"] = True
+
     @app.route("/")
     def index():
-        return "Welcome to OpenHangar", 200
+        return render_template("index.html")
 
     @app.route("/health")
     def health():
@@ -14,4 +18,4 @@ def create_app():
     return app
 
 if __name__ == "__main__":
-    create_app().run(host="0.0.0.0", port=5000)
+    create_app().run(host="0.0.0.0", port=5000, debug=True)
